4649dc680598226b964c0b59f90d6a08e3483705
ikulagin <ivan.i.kulagin@gmail.com>
2015-11-16 19:36:59 +0600

    some renaming + add entry tm_analyzer

--- tm_gcc_plugins/tm_analyzer.cpp
+++ tm_gcc_plugins/tm_analyzer.cpp
@@ -36,18 +36,10 @@ int plugin_is_GPL_compatible;
 
 extern struct ht *ident_hash;
 
-/* static tree */
-/* op_handler (tree *tp, int *walk_subtrees, void *data) */
-/* { */
-/*     printf("\t\t"); */
-/*     print_generic_stmt(stdout, *tp, TDF_RAW | TDF_DETAILS); */
-/*     return NULL; */
-/* } */
-
 const pass_data pass_data_tm_analyze =
     {
         GIMPLE_PASS, /* type */
-        "tm_analyze", /* name */
+        "tm_analyzer", /* name */
         OPTGROUP_INLINE, /* optinfo_flags */
         TV_NONE, /* tv_id */
         PROP_trees, /* properties_required */
@@ -66,36 +58,7 @@ class pass_tm_analyze : public gimple_opt_pass
 
     /* opt_pass methods: */
     virtual unsigned int execute (function *);
-    
-    virtual bool gate(function *);
-
-}; // class pass_warn_self_assign
-
-/* static tree seq_no_tm(gimple_stmt_iterator *gsi, bool *flag, */
-/*                       struct walk_stmt_info *wi) */
-/* { */
-/*     gimple stmt = gsi_stmt(*gsi); */
-/*     enum gimple_code stmt_code = gimple_code(stmt); */
-/*     unsigned n_op = gimple_num_ops(stmt); */
-
-/*     *flag = true; */
-/*     printf("[%s:%d] %s has %u ops <", __func__, __LINE__, */
-/*            gimple_code_name[stmt_code], n_op); */
-
-/*     for (unsigned i = 0; i < n_op; i++) { */
-/*         tree op = gimple_op(stmt, i); */
-/*         if (op != NULL) { */
-/*             printf("[%u]: %s", i, get_tree_code_name(TREE_CODE(op))); */
-/*         } */
-/*     } */
-/*     printf(">\n"); */
-
-/*     if (stmt_code == GIMPLE_TRANSACTION) { */
-/*         printf("section was found\n"); */
-/*     } */
-
-/*     return NULL; */
-/* } */
+}; 
 
 int get_size_operated_mem(int fn_code)
 {
@@ -258,39 +221,6 @@ pass_tm_analyze::execute(struct function *fun)
         }
     }
     printf("%s exit\n", function_name(fun));
-    /* gimple_seq body = gimple_body(fun->decl); */
-
-    /* if (body == NULL) */
-    /*     printf("body is NULL\n"); */
-
-    /* printf("Entering %s\n", function_name(fun)); */
-    /* memset(&wi, 0, sizeof(struct walk_stmt_info)); */
-    /* walk_gimple_seq_mod(&body, seq_no_tm, NULL, &wi); */
-
-    /* printf("Leaving %s\n", function_name(fun)); */
-    /* basic_block bb; */
-    /* gimple_stmt_iterator gsi; */
-    /* gimple stmt; */
-    /* edge e; */
-    /* edge_iterator ei; */
-    /* struct walk_stmt_info wi; */
-    
-
-    /* FOR_EACH_BB_FN(bb, fun) { */
-    /*     printf("\tSTART Basic block %d\n", bb->index); */
-    /*     for (gsi = gsi_start_bb(bb); !gsi_end_p(gsi); gsi_next(&gsi)) { */
-    /*         stmt = gsi_stmt(gsi); */
-    /*         //            printf("\tnops = %d: ", gimple_num_ops(stmt)); */
-    /*         printf("\t\t"); */
-    /*         print_gimple_stmt(stdout, stmt, 0, TDF_RAW); */
-    /*         //            walk_gimple_op(stmt, op_handler, &wi); */
-    /*         memset(&wi, 0, sizeof(struct walk_stmt_info)); */
-    /*     } */
-    /*     FOR_EACH_EDGE(e, ei, bb->succs) { */
-    /*         printf("\t\tsrc %d -> dst %d\n", e->src->index, e->dest->index); */
-    /*     } */
-    /*     printf("\tEND Basic block %d\n", bb->index); */
-    /* } */
 
     return 0;
 }
@@ -302,82 +232,10 @@ int ht_ident_cb(struct cpp_reader *reader, hashnode ht_node, const void *v)
     return 1;
 }
 
-static const char * const symtab_type_names[] = {"symbol", "function", "variable"};
-
-bool
-pass_tm_analyze::gate(function *fn)
-{
-    /* printf("decl = [ %s ]\n", (const char *) get_tree_code_name(TREE_CODE(decls))); */
-
-    //    printf("%s:%d\n", __func__, __LINE__);
-
-    //    ht_forall(ident_hash, ht_ident_cb, NULL);
-
-    //    symtab_node *node;
-
-    //    FOR_EACH_FUNCTION(node) {
-    /* for (node = symtab->nodes; node != NULL; node = node->next) { */
-    /*     printf("symbol name = [ %s ]\n", node->name()); */
-    /*     //        printf("function = [ %s ", function_name(node->get_fun())); */
-    /* } */
-
-    /* printf("all_translation_units->length() = %u\n", all_translation_units->length()); */
-
-    /* tree t, n; */
-    /* unsigned i; */
-
-    /* FOR_EACH_VEC_ELT(*all_translation_units, i, t) { */
-    /*     printf("all_translation_units tree.code = [ %s ]\n", get_tree_code_name(TREE_CODE(t))); */
-    /*     for (n = t; n; n = TREE_CHAIN (n)) { */
-    /*         printf("for()... code = %s\n", get_tree_code_name(TREE_CODE(n))); */
-    /*         if (TREE_CODE(n) == TRANSLATION_UNIT_DECL) { */
-                
-    /*             tree nextp = BLOCK_VARS(DECL_INITIAL(n)); */
-
-    /*             while (nextp) { */
-    /*                 tree var = nextp; */
-    /*                 tree decl_name = DECL_NAME(var); */
-    /*                 printf("decl_name_code = %s", get_tree_code_name(TREE_CODE(var))); */
-    /*                 nextp = TREE_CHAIN(var); */
-    /*                 if (decl_name != NULL) { */
-    /*                     const char *name = IDENTIFIER_POINTER(decl_name); */
-    /*                     if (name != NULL) */
-    /*                         printf(" name = %s", name); */
-    /*                 } */
-    /*                 printf("\n"); */
-                
-    /*             } */
-    /*         /\* tree name = n->decl_minimal.name; *\/ */
-
-    /*         /\* printf("n = [ %s ]\n", get_tree_code_name(TREE_CODE(n))); *\/ */
-    /*         /\* if (name == NULL) { *\/ */
-    /*         /\*     printf("ERROR\n"); *\/ */
-    /*         /\* } *\/ */
-
-    /*         /\* printf("decl_name_code = %s\n", get_tree_code_name(TREE_CODE(DECL_NAME(n)))); *\/ */
-    /*         /\* const char *str = IDENTIFIER_POINTER(DECL_NAME(n)); *\/ */
-    /*         /\* if (str)  *\/ */
-    /*         /\*     printf("decl_name(n) = [ %s ]\n", str); *\/ */
-    /*         } */
-    /*     } */
-    /* } */
-    return true;
-}
-
 static gimple_opt_pass *
 make_pass_tm_analyze(gcc::context *ctxt)
 {
     return new pass_tm_analyze (ctxt);
-}
-
-void plugin_finish_decl(void *gcc_data, void *user_data)
-{
-    symtab_node *n;
-
-    FOR_EACH_SYMBOL(n) {
-        printf("name = [ %s ]\n", n->name());
-    }
-
 }
 
 int
@@ -399,8 +257,5 @@ plugin_init (struct plugin_name_args *plugin_info,
 
     register_callback (plugin_info->base_name, PLUGIN_PASS_MANAGER_SETUP, NULL, &pass_info);
     
-    /* register_callback (plugin_info->base_name, PLUGIN_PASS_EXECUTION, */
-    /*                    plugin_finish_decl, NULL); */
-
     return 0;
 }
